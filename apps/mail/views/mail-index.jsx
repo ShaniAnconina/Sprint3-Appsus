b9d4ca8b813d8cb8c0833ddee350deb36dedb76d
Shani Anconina <shanianconina310@gmail.com>
2022-12-28 14:17:21 +0200

    implement some fixes

--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -1,13 +1,6 @@
 
 export function MailIndex() {
     
-    check:
-    - long-txt
-    - header
-    - css-folders
-    - note-app // like note-index?
-    - email-app // like email-index?
-    - font-awsome
     
     return <div>mail app</div>
 }
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -6,6 +6,8 @@ import { About } from "./views/about.jsx"
 import { Home } from "./views/home.jsx"
 import { MailIndex } from "./apps/mail/views/mail-index.jsx"
 import { NoteIndex } from "./apps/note/views/note-index.jsx"
+import { Services } from "./views/services.jsx"
+import { Team } from "./views/team.jsx"
 
 
 
@@ -19,7 +21,6 @@ export function App() {
                     <Route element={<Services />} path="/about" />
                     <Route element={<Team />} path="/about/team" />
                 </Route>
-
                 <Route path="/mail" element={<MailIndex />} />
                 <Route path="/note" element={<NoteIndex />} />
             </Routes>
